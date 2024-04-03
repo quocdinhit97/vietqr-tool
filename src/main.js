810d0d630414aeaab93fa7e1096f502804a6a30a
Dinh.PhungQuoc <Dinh.PhungQuoc@homecredit.net>
2024-04-03 15:58:19 +0700

    scan qr

--- src/main.js
+++ src/main.js
@@ -2,7 +2,24 @@ import { QRPay } from './qr-pay.js';
 
 const inputArea = document.querySelector(".large-area--input");
 const outputArea = document.querySelector(".large-area--output");
-const btnClear = document.querySelector(".controls_button--clear");
+const btnScan = document.querySelector(".controls_button--scan");
+const qrScan = document.querySelector("#popup");
+const btnClose = document.querySelector(".close");
+
+
+
+var html5QrcodeScanner = new Html5QrcodeScanner(
+	"reader", { fps: 10, qrbox: 250});
+html5QrcodeScanner.render(onScanSuccess);
+
+function onScanSuccess(decodedText, decodedResult) {
+    inputArea.value = decodedText;
+    const qrPay = new QRPay(inputArea.value);
+    const formatted = JSON.stringify(qrPay, null, 4)
+    outputArea.value = formatted;
+    qrScan.classList.toggle("active");
+}
+
 
 inputArea.addEventListener("input", () => {
     const qrPay = new QRPay(inputArea.value);
@@ -10,7 +27,10 @@ inputArea.addEventListener("input", () => {
     outputArea.value = formatted;
 })
 
-btnClear.addEventListener("click", () => {
-    outputArea.value = "";
-    inputArea.value = "";
+btnScan.addEventListener("click", () => {
+    qrScan.classList.toggle("active");
+})
+
+btnClose.addEventListener("click", () =>  {
+    qrScan.classList.toggle("active");
 })
